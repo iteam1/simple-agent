4ef31369faa2e103ffe76dfd896e428f712c00a5
locch <locc7196@gmail.com>
2025-03-23 02:25:15 +0700

    init

---
+++ app.py
@@ -0,0 +1,23 @@
+import pytz
+import yaml
+import datetime
+import requests
+#from tools.final_answer import FinalAnswerTool
+#from UI import GradioUI
+from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel, load_tool, tool
+
+
+# Create Tools
+
+# Import a text-to-image generation tool from the Hugging Face Hub
+#image_generation_tool = load_tool("agents-course/text-to-image", trust_remote_code=True)
+
+# Initialize the FinalAnswerTool
+
+# Use a Hugging Face Endpoint
+
+# Load prompt templates from a YAML file
+
+# Initialize a CodeAgent instance, which will use the model and tools
+
+# Launch a Gradio UI to interact with the agent
